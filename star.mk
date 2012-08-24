ca691d1d0ae7aaaa36d854ce1221a9676572c53b
tonypp <text2tony@gmail.com>
2012-08-24 20:37:24 +0300

    use MTP as default storage configuration

--- star.mk
+++ star.mk
@@ -91,8 +91,8 @@ PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
 
 # Set default USB interface
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	persist.sys.usb.config=ptp
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_MANUFACTURER := LGE
